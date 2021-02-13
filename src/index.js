0cda1f8f028ec75b7af129dcb7f468a3ee889980
Melissa Danielle Myatt <mdm@Melissas-Air.attlocal.net>
2021-02-12 17:58:29 -0800

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { choice, remove } from './helpers'
+import fruits from './foods'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const RANDOMFRUIT = choice(fruits);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(`I’d like one ${RANDOMFRUIT}, please.`);
+console.log(`Here you go: ${RANDOMFRUIT}`);
+console.log(`Delicious! May I have another?`);
+
+const FRUITSLEFT = remove(RANDOMFRUIT, fruits);
+
+console.log(`I’m sorry, we’re all out. We have ${FRUITSLEFT.length} left.`)
+
